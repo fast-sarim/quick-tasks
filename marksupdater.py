54d72feb452047efec197cfb9402d4e6700dc24b
Muhammad Sarim <sarimbinsaleh@gmail.com>
2026-01-09 21:34:04 +0500

    Add files via upload

---
+++ marksupdater.py
@@ -0,0 +1,9 @@
+marks = {}
+
+marks["math"] = int(input("enter marks of math: "))
+marks["physics"] = int(input("enter marks of physics: "))
+marks["computer"] = int(input("enter marks of computer: "))
+marks["english"] = int(input("enter marks of english: "))
+marks["chemistry"] = int(input("enter marks of chemistry: "))
+
+print(marks)
